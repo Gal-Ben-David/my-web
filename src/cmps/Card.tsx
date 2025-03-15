bc95c58dac632c2ca81539d35c695d08b9fb8bbd
Gal Ben David <gal.benda3@gmail.com>
2025-03-15 16:29:44 +0200

    Add github and linkedin links to Card cmp

--- src/cmps/Card.tsx
+++ src/cmps/Card.tsx
@@ -4,8 +4,20 @@ export function Card() {
             <div className="card-details">
                 <h2>Gal Ben David</h2>
                 <h3>Full Stack Developer</h3>
-                <button className="btn btn-light download-btn">Download resume</button>
+
+                <div className="links">
+                    <button className="btn btn-light download-btn">Download resume</button>
+                    <a href="https://github.com/Gal-Ben-David" target="_blank">
+                        <img className="github-icon" src="/img/intro/github.png" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/gal-ben-david-202158233/" target="_blank">
+                        <img className="linkedin-icon" src="/img/intro/linkedin.png" />
+                    </a>
+                </div>
+
             </div>
+
+
         </section>
 
     )
