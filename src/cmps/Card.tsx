d04cee62bcd652f857fc2f37fe805bc65272f761
Gal Ben David <gal.benda3@gmail.com>
2025-03-15 15:54:53 +0200

    Implement Card cmp

---
+++ src/cmps/Card.tsx
@@ -0,0 +1,12 @@
+export function Card() {
+    return (
+        <section className="card">
+            <div className="card-details">
+                <h2>Gal Ben David</h2>
+                <h3>Full Stack Developer</h3>
+                <button className="btn btn-light download-btn">Download resume</button>
+            </div>
+        </section>
+
+    )
+}
--- src/cmps/Intro.tsx
+++ src/cmps/Intro.tsx
@@ -1,11 +1,10 @@
+import { Card } from "./Card"
 
 export function Intro() {
 
     return (
         <section className="introduction full">
-            <div className="main-cover-container">
-                <img src="/img/intro/gal-intro-main-cover.jpg" />
-            </div>
+            <Card />
         </section>
     )
 }
